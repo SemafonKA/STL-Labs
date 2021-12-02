deb700bfaf04f0c02289aeb66686667dafac2a21
Semyon <s_se@ngs.ru>
2021-12-02 15:29:16 +0700

    Вроде work

--- STL-Labs/Main.cpp
+++ STL-Labs/Main.cpp
@@ -1,97 +1,75 @@
 #include <iostream>
 #include <vector>
-#include <list>
 #include <queue>
-#include <set>
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define sz(x) (int)x.size()
+
+using pii = std::pair<int, int>;
 
 using namespace std;
 
-struct WeightedEdge
-{
-   size_t weight;
-   size_t firstVertex;
-   size_t secondVertex;
-};
-
-bool operator< (const WeightedEdge& first, const WeightedEdge& second)
-{
-   return first.weight < second.weight;
-}
-
-struct WeightedGraph
-{
-   vector<WeightedEdge> edges;
-   set<size_t> vertices;
-
-   void addEdge(WeightedEdge edge)
-   {
-      edges.push_back(edge);
-      vertices.insert(edge.firstVertex);
-      vertices.insert(edge.secondVertex);      
-   }
-};
-
-WeightedGraph PrimaALgMinimalTree(WeightedGraph initialGraph)
-{
-   list<size_t> unchoosedVertices;
-   for (auto& vertex : initialGraph.vertices)
-   {
-      unchoosedVertices.push_back(vertex);
-   }
-
-   list<size_t> choosedVertices;
-   priority_queue<WeightedEdge> edges;
-
-   while (unchoosedVertices.size() != 0)
-   {
-      // Если недобавленных рёбер нет, то добавляем в список вершину, у которой
-      // должны найтись недобавленные рёбра
-      if (edges.size() == 0)
-      {
-         auto vertex = unchoosedVertices.front();
-         choosedVertices.push_back(vertex);
-         unchoosedVertices.pop_front();
-
-         // Добавляем все рёбра, связанные с этой вершиной
-         for (auto& edge : initialGraph.edges)
-         {
-            if (edge.firstVertex == vertex || edge.secondVertex == vertex)
-            {
-               edges.push(edge);
-            }
-         }
-      }
-      
-   }
-
-   return initialGraph;
-}
-
 int main()
 {
-   //size_t numVertices, numEdges;
-   //cin >> numVertices >> numEdges;
+   int n, m;
 
-   //WeightedGraph initialGraph;
-   //// Считали граф с файла по рёбрам
-   //for (size_t i = 0; i < numEdges; i++)
-   //{
-   //   WeightedEdge edge;
-   //   cin >> edge.firstVertex >> edge.secondVertex >> edge.weight;
-   //   edge.firstVertex--; edge.secondVertex--;
-   //   initialGraph.addEdge(edge);
-   //}
+   cin >> n >> m;
 
-   priority_queue<WeightedEdge> que;
-   WeightedEdge edge; edge.firstVertex = 1; edge.secondVertex = 2, edge.weight = 4;
-   que.push(edge);
-   edge.firstVertex = 1; edge.secondVertex = 3, edge.weight = 6;
-   que.push(edge);
+   vector < vector < pii > > g(n + 1);
 
-   while (!que.empty())
+   for (int i = 1; i <= m; ++i)
    {
-      edge = que.top();
-      que.pop();
-      cout << edge.weight << " " << edge.firstVertex << " " << edge.secondVertex << endl;
+      int u, v, w;
+      cin >> u >> v >> w;
+      g[u].pb(mp(v, w));
+      g[v].pb(mp(u, w));
    }
+
+   priority_queue <pii, vector < pii >, greater < pii> > q;
+
+   vector < int > key(n + 1, INT_MAX);
+   vector < int > p(n + 1, -1);
+   vector < bool > was(n + 1, false);
+
+
+   int start = 1;
+
+   q.push(mp(0, start));
+
+   int min_w = 0;
+
+   while (!q.empty())
+   {
+      int len = q.top().F;
+      int v = q.top().S;
+
+      q.pop();
+
+      if (was[v])
+         continue;
+
+      was[v] = true;
+      min_w += len;
+
+      for (auto cur : g[v])
+      {
+         int to = cur.F, w = cur.S;
+         if (!was[to] && key[to] > w)
+         {
+            key[to] = w;
+            q.push(mp(key[to], to));
+            p[to] = v;
+         }
+      }
+   }
+
+   cout << min_w << endl;
+
+   for (int i = 2; i <= n; ++i)
+      cout << p[i] << " - " << i << endl;
+
+   return 0;
 }
