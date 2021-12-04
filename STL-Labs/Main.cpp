7651e785f5c41185037a02d62244906767872eba
Semyon <s_se@ngs.ru>
2021-12-04 10:04:13 +0700

    Ок

--- STL-Labs/Main.cpp
+++ STL-Labs/Main.cpp
@@ -1,73 +1,57 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define sz(x) (int)x.size()
-
-using pii = std::pair<int, int>;
 
 using namespace std;
 
-int main()
+class WeighedEdge
 {
-   int n, m;
-   cin >> n >> m;
+public:
+   size_t firstNode;
+   size_t secondNode;
+   size_t weight;
 
-   vector < vector < pii > > g(n + 1);                            // начальный граф (0 элемент не используется) (pii используется вида конецРебра - весРебра)
-
-   for (int i = 1; i <= m; ++i)
+   WeighedEdge(size_t _firstNode, size_t _secondNode, size_t _weight)
    {
-      int u, v, w;
-      cin >> u >> v >> w;
-      g[u].pb(mp(v, w));
-      g[v].pb(mp(u, w));
+      firstNode = _firstNode;
+      secondNode = _secondNode;
+      weight = _weight;
    }
 
-   priority_queue < pii, vector < pii >, greater < pii > > q;     // Очередь вес-вершина
-   vector < int > key(n + 1, INT_MAX);                            // Храним вес минимального ребра, исходящего из выбранной вершины
-   vector < int > p(n + 1, -1);                                   // Храним вершину, куда указывает минимальное ребро из выбранной вершины
-   vector < bool > was(n + 1, false);                             // Для отметок, прошли ли мы вершину
-
-
-   int start = 1;                                                 // Начальная вершина обхода
-   int min_w = 0;                                                 // Вес минимального графа
-
-   q.push(mp(0, start));
-
-   while (!q.empty())
+   bool operator == (const WeighedEdge& _right) const
    {
-      int len = q.top().F;                                        // Длина ребра
-      int v = q.top().S;                                          // Вершина, куда ребро привело
-
-      q.pop();
-
-      if (was[v])                                                 // Если уже были в этой вершине, то пропускаем ребро
-         continue;
-
-      was[v] = true;                                              // Помечаем вершину как пройденную, считаем вес ребра, который вёл до неё
-      min_w += len;
-
-      for (auto& cur : g[v])                                      // Находим все рёбра этой вершины
-      {
-         int to = cur.F;                                          // Вершина, куда текущее ребро ведёт
-         int w = cur.S;                                           // Вес текущего ребра
-         if (!was[to] && key[to] > w)                             // Если мы ещё не были в вершине и вес минимального ребра этой вершины больше, чем вес текущего ребра, то
-         {
-            key[to] = w;                                          // Запоминаем для этой вершины вес минимального ребра   
-            q.push(mp(w, to));                                    // Пушим вершину с весом до неё в очередь
-            p[to] = v;                                            // Отмечаем, что из этой вершины можно попасть в вершину [v]
-         }
-      }
+      return (firstNode == _right.firstNode && 
+         secondNode == _right.secondNode && 
+         weight == _right.weight);
    }
 
-   cout << min_w << endl;
+   bool operator < (const WeighedEdge& _right) const
+   {
+      return weight < _right.weight;
+   }
+   bool operator > (const WeighedEdge& _right) const
+   {
+      return weight > _right.weight;
+   }
 
-   for (int i = 2; i <= n; ++i)
-      cout << p[i] << " - " << i << endl;
+   bool contains(size_t _node)
+   {
+      return (firstNode == _node || secondNode == _node);
+   }
+};
 
-   return 0;
-}
+class WeighedGraph
+{
+public:
+   vector<WeighedEdge> edges;
+
+   void addEdge(const WeighedEdge _edge)
+   {
+      edges.push_back(_edge);
+   }
+
+   bool removeEdge(const WeighedEdge _edge)
+   {
+      std::swap(edges[edges.size() - 1], *find(edges.begin(), edges.end(), _edge));
+      edges.pop_back();
+   }
+};
