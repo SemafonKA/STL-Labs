3ae9c90c1c20914979b82f8e6302569c0d0c60af
Semyon <61237950+SemafonKA@users.noreply.github.com>
2021-12-21 19:09:08 +0700

    Параллельная

--- STL-Labs/Main.cpp
+++ STL-Labs/Main.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <vector>
 #include <omp.h>
+#include <chrono>
+
+#pragma region Trash)))
 
 #define per(name, beg, en) for (int name = beg; name <= en; ++name)
 #define rep(name, beg, en) for (int name = beg; name >= en; --name)
@@ -17,20 +20,60 @@
 #define SF second.first
 #define SS second.second
 
+#pragma endregion
+
 using namespace std;
 
+class Timer
+{
+private:
+   // Псевдонимы типов используются для удобного доступа к вложенным типам
+   using clock_t = std::chrono::high_resolution_clock;
+   using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+   std::chrono::time_point<clock_t> m_beg;
+   double last_elapsed;
+public:
+   Timer() : m_beg(clock_t::now()), last_elapsed(0)
+   {
+   }
+
+   void reset()
+   {
+      m_beg = clock_t::now();
+      last_elapsed = 0.0;
+   }
+
+   double elapsed()
+   {
+      last_elapsed = std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+      return last_elapsed;
+   }
+
+   inline double getLastElapsed()
+   {
+      return last_elapsed;
+   }
+};
 
 using pii = pair<int, int>;
 
-using GraphList = vector<vector<pii>>;      // Граф списком смежности
+using GraphList = vector<vector<pii>>;       // Граф списком смежности
 using GraphMatrix = vector<vector<int>>;     // Граф матрицей смежности
 
 const int inf = INT_MAX;
 const pii pinf = mp(inf, inf);
 
-pii finder(vector < pii > vec, int v) {
+/// <summary>
+/// Находит ребро с конечной вершиной [v] в списке смежности [vec] от начальной вершины
+/// </summary>
+/// <param name="vec"> - список смежности для начальной вершины</param>
+/// <param name="v"> - конечная вершина</param>
+/// <returns></returns>
+inline pii finder(const vector < pii >& vec, int v)
+{
    pii ans = pinf;
-   for (auto x : vec)
+   for (const auto& x : vec)
       if (x.F == v)
       {
          ans = x;
@@ -39,23 +82,49 @@ pii finder(vector < pii > vec, int v) {
    return ans;
 }
 
-void floyd(GraphMatrix sD, int size)
+inline void matrixPrinter(const GraphMatrix& graph, ostream& out)
 {
-   per(i, 1, size - 1)
-      per(u, 1, size - 1)
-         per(v, 1, size - 1)
-         if (sD[u][i] < inf && sD[i][v] < inf)
-            sD[u][v] = min(sD[u][v], sD[u][i] + sD[i][v]);
-
+   size_t size = graph.size();
    per(u, 1, size - 1)
    {
       per(v, 1, size - 1)
-         cout << ((sD[u][v] == inf || u == v) ? 0 : sD[u][v]) << ' ';
-      cout << endl;
+         out << ((graph[u][v] == inf || u == v) ? 0 : graph[u][v]) << " \t";
+      out << endl;
    }
 }
 
-void floydAlgMatrix(const GraphMatrix& graph)//, ostream& out, int numThreads = omp_get_max_threads())
+inline void listPrinter(const GraphMatrix& graph, ostream& out)
+{
+   size_t size = graph.size();
+   per(u, 1, size - 1)
+   {
+      per(v, 1, size - 1)
+      {
+         if (u != v && graph[u][v] != inf)
+         {
+            out << u << " \t" << v << " \t" << graph[u][v] << endl;
+         }
+      }
+   }
+}
+
+/// <summary>
+/// Алгоритм Флойда от матрицы смежности
+/// </summary>
+/// <param name="sD"> Начальная матрица смежности, она же результат работы</param>
+inline void _floyd(GraphMatrix& sD, int numThreads = omp_get_max_threads())
+{
+   size_t size = sD.size();
+
+#pragma omp parallel for num_threads(numThreads) schedule(dynamic, 200)
+   per(i, 1, size - 1)
+      per(u, 1, size - 1)
+         per(v, 1, size - 1)
+            if (sD[u][i] < inf && sD[i][v] < inf)
+               sD[u][v] = min(sD[u][v], sD[u][i] + sD[i][v]);
+}
+
+void floydAlgMatrix(const GraphMatrix& graph, ostream& out, int numThreads = omp_get_max_threads())
 {
    GraphMatrix sD = graph;
    int size = static_cast<int>(sz(graph));
@@ -65,10 +134,12 @@ void floydAlgMatrix(const GraphMatrix& graph)//, ostream& out, int numThreads = 
       if (!sD[u][v])
          sD[u][v] = inf;
 
-   floyd(sD, size);
+   _floyd(sD, numThreads);
+
+   listPrinter(sD, out);
 }
 
-void floydAlgList(const GraphList& graph)
+void floydAlgList(const GraphList& graph, ostream& out, int numThreads = omp_get_max_threads())
 {
    GraphMatrix sD;
    int size = static_cast<int>(sz(graph));
@@ -79,33 +150,64 @@ void floydAlgList(const GraphList& graph)
       per(j, 1, size - 1)
          sD[i].pb(finder(graph[i], j).S);
    }
-   floyd(sD, size);
+
+   _floyd(sD, numThreads);
+
+   listPrinter(sD, out);
 }
 
-void solve()
+// Ввод данных и вызов методов
+void solve(int num_threads)
 {
+   ifstream in("input.txt");
+   if (!in.is_open()) throw exception("File input.txt wasn't open!");
+
    int n, m;
-   cin >> n >> m;
-   GraphMatrix g;       // граф матрицей смежности
-   GraphList G;         // граф списком смежности
-   G.resize(n + 1);
-   g.resize(n + 1);
+   in >> n >> m;
+
+   GraphMatrix matrixGraph;         // граф матрицей смежности
+   GraphList listGraph;             // граф списком смежности
+   listGraph.resize(n + 1);
+   matrixGraph.resize(n + 1);
    per(i, 1, n)
-      g[i].resize(n + 1, 0);
+      matrixGraph[i].resize(n + 1, 0);
+
    per(i, 1, m)
    {
       int u, v, w;
-      cin >> u >> v >> w;
-      g[u][v] = g[v][u] = w;
-      G[u].pb(mp(v, w));
-      G[v].pb(mp(u, w));
+      in >> u >> v >> w;
+      matrixGraph[u][v] = matrixGraph[v][u] = w;
+      listGraph[u].pb(mp(v, w));
+      listGraph[v].pb(mp(u, w));
    }
-   floydAlgMatrix(g);
-   cout << endl;
-   floydAlgList(G);
+   in.close();
+
+   ofstream outMatrix("outMatrix.txt");
+   Timer timer;
+   floydAlgMatrix(matrixGraph, outMatrix, num_threads);
+   timer.elapsed();
+   cout << "Алгоритм по матрице завершил работу за " << timer.getLastElapsed() << " секунд" << endl;
+   outMatrix.close();
+
+   ofstream outList("outList.txt");
+   timer.reset();
+   floydAlgList(listGraph, outList, num_threads);
+   timer.elapsed();
+   cout << "Алгоритм по листу завершил работу за " << timer.getLastElapsed() << " секунд" << endl;
+   outList.close();
 }
 
 int main()
 {
-   solve();
+   setlocale(LC_ALL, "ru");
+   vector<int> numThreadsList = {  };
+   for (int i = 4; i <= omp_get_max_threads(); i += 2) numThreadsList.push_back(i);
+
+   for (auto& numThreads : numThreadsList)
+   {
+      cout << "************************************" << endl;
+      cout << "Работа алгоритмов для [" << numThreads << "] потоков: " << endl << endl;
+      solve(numThreads);
+      cout << "************************************" << endl << endl;
+   }
 }
